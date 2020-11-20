dff1a0503cdcb31428fbbe3aeb7fd75296c53955
Todd Guo <1741279100@qq.com>
2020-11-20 20:26:19 +0800

    init repo

---
+++ src/test/java/life/daguobiji/community/CommunityApplicationTests.java
@@ -0,0 +1,12 @@
+package life.daguobiji.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplicationTests {
+	public static void main(String []args) {
+		SpringApplication.run(CommunityApplication.class,args);
+	}
+
+}
